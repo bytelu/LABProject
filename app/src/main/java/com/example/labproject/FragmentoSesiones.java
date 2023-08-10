500118a6927d9464fff6c8ed2ae0512c6de23b32
LuisDCruzC <luis.david.9960@gmail.com>
2023-08-10 15:06:04 -0600

    Prueba Crear Sesion

--- app/src/main/java/com/example/labproject/FragmentoSesiones.java
+++ app/src/main/java/com/example/labproject/FragmentoSesiones.java
@@ -7,8 +7,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.labproject.sesindividual.SesionIndividual;
 
 public class FragmentoSesiones extends Fragment {
+    private boolean esRegistro = false; // Variable para controlar qué fragmento se muestra
 
     public FragmentoSesiones() {
         // Required empty public constructor
@@ -16,6 +20,36 @@ public class FragmentoSesiones extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_fragmento_sesiones, container, false);
+        View view =  inflater.inflate(R.layout.fragment_fragmento_sesiones, container, false);
+
+        // Referencias a las pestañas
+        TextView tabSesionIndividual = view.findViewById(R.id.tabSesionIndividual);
+        TextView tabSesionGrupal = view.findViewById(R.id.tabSesionGrupal);
+
+        tabSesionIndividual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cambiarFragmento(new FragmentoCrearIndividual());
+            }
+        });
+
+        tabSesionGrupal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cambiarFragmento(new FragmentoCrearGrupal());
+            }
+        });
+
+        if(savedInstanceState == null){
+            cambiarFragmento(new FragmentoCrearGrupal());
+        }
+        return view;
+    }
+
+    //Metodo para cambiar fragmentos
+    private void cambiarFragmento(Fragment fragment){
+        getChildFragmentManager().beginTransaction()
+                .replace(R.id.fragmentContenedor, fragment)
+                .commit();
     }
 }
