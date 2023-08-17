10956e92aef58018d5b6cb500da25e4044a55dd4
bytelu <luan78hihe@gmail.com>
2023-08-17 14:54:34 -0600

    Mejorando Layout de Crear Reportes y agregando recursos

--- app/src/main/java/com/example/labproject/FragmentoCrearReporte.java
+++ app/src/main/java/com/example/labproject/FragmentoCrearReporte.java
@@ -2,13 +2,12 @@ package com.example.labproject;
 
 import android.os.Bundle;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class FragmentoCrearReporte extends Fragment {
 
@@ -17,18 +16,18 @@ public class FragmentoCrearReporte extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_fragmento_crear_reporte, container, false);
 
-        FloatingActionButton fabRegresarReporte = view.findViewById(R.id.regresarReporte);
+        ConstraintLayout fabRegresarReporte = view.findViewById(R.id.regresarReporte);
+
         fabRegresarReporte.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Reemplaza "FragmentoDestino" con el nombre de tu fragmento de destino
                 FragmentoReportes fragmentoDestino = new FragmentoReportes();
-                getActivity().getSupportFragmentManager()
+                requireActivity().getSupportFragmentManager()
                         .beginTransaction()
                         .replace(R.id.fragment_container, fragmentoDestino) // Reemplaza fragment_container con el ID de tu contenedor de fragmentos
                         .addToBackStack(null) // Opcional: agrega la transacción a la pila de retroceso
