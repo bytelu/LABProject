a07d8aa7ceb89493114dd8d8d22d5a45eec6eb7b
bytelu <luan78hihe@gmail.com>
2023-08-19 17:13:26 -0600

    Actualizacion de minSdkVersion a 24, por compatibilidad de libreria ZXing e implementación de QR

--- app/src/main/java/com/example/labproject/FragmentoCrearIndividual.java
+++ app/src/main/java/com/example/labproject/FragmentoCrearIndividual.java
@@ -1,16 +1,21 @@
 package com.example.labproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class FragmentoCrearIndividual extends Fragment {
 
@@ -39,10 +44,31 @@ public class FragmentoCrearIndividual extends Fragment {
             @Override
             public void onClick(View view) {
                 //Aqui se manda a llamar a la camara y todo lo de web scraping
-
+                IntentIntegrator integrator = IntentIntegrator.forSupportFragment(FragmentoCrearIndividual.this);
+                integrator.setPrompt("Escanear Codigo QR");
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+                integrator.setCameraId(0);
+                integrator.initiateScan();
             }
         });
 
         return view;
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(getActivity(), "Escaneo Cancelado", Toast.LENGTH_SHORT).show();
+            } else {
+                String qrContent = result.getContents();
+                Toast.makeText(getActivity(), "Contenido del QR: " + qrContent, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 }
+
