9fbdf659a11fda2903a896efbe94645ab62b1741
bytelu <luan78hihe@gmail.com>
2023-09-07 17:26:53 -0600

    Cambiando base de datos a MySQL 5, quitando conector 8 y agregando conector 5, e implementando registro de encargado con nueva base de datos y conector.

--- app/src/main/java/com/example/labproject/res/CData.java
+++ app/src/main/java/com/example/labproject/res/CData.java
@@ -2,13 +2,10 @@ package com.example.labproject.res;
 
 public class CData {
 
-    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String SERVER_IP = "localhost";
-    private static final String PORT = "3306"; // Puerto MySQL por defecto
-    private static final String DATABASE_NAME = "db_laboratorio";
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
     private static final String USERNAME = "Encargado";
     private static final String PASSWORD = "ENCARGADO";
-    private static final String URL = "jdbc:mysql://" + SERVER_IP + ":" + PORT + "/" + DATABASE_NAME;
+    private static final String URL = "jdbc:mysql://192.168.1.75/db_laboratorio";
     public static String getDriver(){
         return DRIVER;
     }
